4725df7320aaf1b4091a313f5c4dae63761a9786
EnshteinDe <sonikovskiy@gmail.com>
2025-04-24 18:01:41 +0300

    Home, pink_container

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <h1 className="font-work-sans text-2xl">Home page</h1>;
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+      </section>
+    </>
+  );
 }
