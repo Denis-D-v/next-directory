9c81010de5b0ab0e15a7763d645ef2a46b0f077e
EnshteinDe <sonikovskiy@gmail.com>
2025-04-29 02:02:26 +0300

    create StartupCard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import SearchForm from '@/components/SearchForm';
+import StartupCard from '@/components/StartupCard';
 
 export default async function Home({
   searchParams,
@@ -6,7 +7,20 @@ export default async function Home({
   searchParams: Promise<{ query?: string }>;
 }) {
   const query = (await searchParams).query;
-  const params = { search: query || null };
+  // const params = { search: query || null };
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: 'Albert' },
+      _id: 1,
+      description: 'This is a description.',
+      image:
+        'https://www.moriareviews.com/rongulator/wp-content/uploads/Short-Circuit-1986-11.jpg',
+      category: 'Robots',
+      title: 'We Robots',
+    },
+  ];
 
   return (
     <>
@@ -21,6 +35,20 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType, index: number) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
       </section>
     </>
   );
