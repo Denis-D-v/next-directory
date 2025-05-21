ee8a79ef3d8d93560c695b1a4c78c1b437ed6b3b
EnshteinDe <sonikovskiy@gmail.com>
2025-05-22 02:20:28 +0300

    sanityFetch, SanityLive, create a new doc

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity';
 import { author } from './author';
 import { startup } from './startup';
-import { markdownSchema } from 'sanity-plugin-markdown';
+// import { markdownSchema } from 'sanity-plugin-markdown';
 
-const schemaTypes: SchemaTypeDefinition[] = [author, startup, markdownSchema];
-
-export default schemaTypes;
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [author, startup],
+};
